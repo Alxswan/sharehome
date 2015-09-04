fd0812ecce4c4945af6fae36a443f5597be2c596
ALEXANDRA SWAN <alexandraswan@gmail.com>
2015-09-04 18:01:56 +1000

    update new user form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,5 +6,30 @@ class UsersController < ApplicationController
   end
 
   def new
+  	@user = User.new
   end
+
+  def create
+  	@user = User.new user_params
+  	if @user.save
+  		redirect_to root_path
+  	else
+  		render :new
+  	end
+  end
+
+  private 
+
+  def user_params
+  	params.require(:user).permit(:email, :first_name, :last_name :password, :password_confirmation)   	
+  end
+
+  def check_if_logged_in
+  	redirect_to root_path unless @current_user.present?
+  end
+
+  def check_if_admin
+  	redirect_to root_path unless @current_user.present? && @current_user.admin?
+  end
+
 end
